68259b7e3c1b1a1c10b2ff1b68f5d512eb4745de
Vivek Kushwaha <vivekkushwaha2727@gmail.com>
2026-07-23 10:17:10 +0530

    Initialized RabbitMQ demo project using Docker

---
+++ Shared/Models/MessageDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Models
+{
+    public class MessageDto
+    {
+        public Guid Id { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
